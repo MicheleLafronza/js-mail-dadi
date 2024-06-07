c3f4239c23b492203db6c66abd6e185b9204e55c
MicheleLafronza <michele.lafronza91@gmail.com>
2024-06-07 17:27:51 +0200

    feat: add output mail check result

--- mail/js/main.js
+++ mail/js/main.js
@@ -2,28 +2,38 @@
 const partyList = ["tonygmail.com", "mike@gmail.com", "luke@gmail.com", "paul@gmail.com", "jack@gmail.com"];
 console.log("Lista invitati", partyList);
 
+// creo variabili per la risposta finale
+let risp = document.querySelector(".risposta");
+let t = 0;
+
 // chiedo all'utente la sua mail con una funzione
 const button = document.querySelector(".btn");
-
 
 button.addEventListener("click",
     function() {
         let inputMail = document.querySelector(".mail").value;
         console.log("mail inserita dall'utente", inputMail);
+        
 
         // controllo con un ciclo ogni mail degli invitati
         for (let i=0; i < partyList.length; i++){
             let mailInv = partyList[i];
             console.log("mail controllo ciclo",mailInv);
 
-            // se la mail dell'utente combacia mando un messaggio di invito
-            
+            // se la mail dell'utente combacia mando un messaggio di invito altrimenti no
             if (inputMail === mailInv) {
-                console.log("La tua mail è tra la lista degli invitati");
-            } else {
-                console.log("mi dispiace");
+                // risp.innerHTML=("Sei invitato alla festa!!!")
+                t = 1;
+                console.log("Sei invitato alla festa!!!");
             }
         }
+
+        if (t === 1) {
+            risp.innerHTML=("Sei invitato alla festa!!!")
+        } else if (t === 0) {
+            risp.innerHTML=("Non sei invitato alla festa!!!")
+        }
+
     }
 )
 
@@ -34,4 +44,3 @@ button.addEventListener("click",
 
 
 
-// se la mail non combacia gli dico che mi dispiace T_T
