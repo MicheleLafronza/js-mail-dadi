691354b67dd6f635e8b19791416ea48818f2ea03
MicheleLafronza <michele.lafronza91@gmail.com>
2024-06-07 16:51:22 +0200

    feat(mail): add cycle mail check

--- mail/js/main.js
+++ mail/js/main.js
@@ -1,21 +1,37 @@
 // preparo un array con le mail degli invitati
 const partyList = ["tonygmail.com", "mike@gmail.com", "luke@gmail.com", "paul@gmail.com", "jack@gmail.com"];
-console.log(partyList);
+console.log("Lista invitati", partyList);
 
 // chiedo all'utente la sua mail con una funzione
 const button = document.querySelector(".btn");
 
+
 button.addEventListener("click",
     function() {
         let inputMail = document.querySelector(".mail").value;
-        console.log(inputMail);
+        console.log("mail inserita dall'utente", inputMail);
+
+        // controllo con un ciclo ogni mail degli invitati
+        for (let i=0; i < partyList.length; i++){
+            let mailInv = partyList[i];
+            console.log("mail controllo ciclo",mailInv);
+
+            // se la mail dell'utente combacia mando un messaggio di invito
+            
+            if (inputMail === mailInv) {
+                console.log("La tua mail è tra la lista degli invitati");
+            } else {
+                console.log("mi dispiace");
+            }
+        }
     }
 )
 
 
 
-// controllo con un ciclo ogni mail degli invitati
 
-// se la mail dell'utente combacia mando un messaggio di invito
+
+
+
 
 // se la mail non combacia gli dico che mi dispiace T_T
